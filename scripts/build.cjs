8d4ea764aa8f1576537a21e4263ed55a0bfb2597
Milos Tomic <milos.tomic@sap.com>
2024-11-05 09:51:33 +0100

    fixes

--- scripts/build.cjs
+++ scripts/build.cjs
@@ -4,13 +4,6 @@ const { globSync } = require('glob');
 rmSync('./dist', { recursive: true, force: true});
 mkdirSync('./dist/src', {recursive: true});
 
-cpSync('./LICENSE', './dist/LICENSE');
-cpSync('./README.md', './dist/README.md');
-
-cpSync('./steps.cjs', './dist/steps.cjs');
-cpSync('./steps.mjs', './dist/steps.mjs');
-cpSync('./lib.cjs', './dist/lib.cjs');
-cpSync('./lib.mjs', './dist/lib.mjs');
 cpSync('./src', './dist/src', {recursive: true});
 
 rmSync('./dist/src/tests', { recursive: true, force: true});
