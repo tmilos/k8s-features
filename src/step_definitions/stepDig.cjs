6a292f37646fd68bdb029b148379db83709387e7
Vladimir Andjelkoski <vladimir.andjelkoski@sap.com>
2025-08-01 11:36:35 +0200

    dig operation

---
+++ src/step_definitions/stepDig.cjs
@@ -0,0 +1,36 @@
+const { Then } = require('@cucumber/cucumber');
+const { DigOptions } = require('../support/dig.cjs');
+
+Then(
+  'dig {string} resolves to {string}',
+  /**
+   * @this import("../support/world.cjs").MyWorld
+   * @param {string} domain
+   * @param {string} ip
+   */
+  async function(url) {
+    const options = new DigOptions();
+    options.domain = domain;
+    options.expectedOutput = ip
+    await this.dig(options);
+  }
+);
+
+/*
+  Then dig operation succeedes:
+    | Domain         | example.com |
+    | ExpectedOutput | something   |
+*/
+
+Then(
+  'dig operation succeedes:',
+  /**
+   * @this import("../support/world.cjs").MyWorld
+   * @param {import("@cucumber/cucumber").DataTable} dataTable
+   */
+  async function(dataTable) {
+    const options = new DigOptions();
+    options.loadFromDataTable(this, dataTable);
+    await this.dig(options);
+  }
+);
